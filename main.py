cf0502a856cb21edce3212993381534dcb65e61b
Yurasblv <74055402+Yurasblv@users.noreply.github.com>
2021-12-27 13:23:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def direction():
     all_directions = ('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW')
     single_side = 45
     try:
+        print('Enter facing')
         facing = str(input().upper())
+        print('Enter turn')
         turn = int(input())
         if facing in all_directions:
             if turn in range(-1080,1080) and turn%single_side==0 :
@@ -17,4 +19,12 @@ def direction():
         print (f"error: {e}")
 
 if __name__ == '__main__':
+    print("""You receive the direction you are facing (one of the 8 directions: N, NE, E, SE, S, SW, W, NW)
+     and a certain degree to turn (a multiple of 45, between -1080 and 1080); positive means clockwise, 
+     and negative means counter-clockwise.
+     Return the direction you will face after the turn.
+     Examples:
+        "S",  180  -->  "N"
+        "SE", -45  -->  "E"
+        "W",  495  -->  "NE""")
     direction()
